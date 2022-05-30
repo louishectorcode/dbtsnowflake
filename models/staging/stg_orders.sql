e89d9b0640a9b63d480d3531a44309e3b6a615f0
appdev87 <appdev@hectorcode.com>
2022-05-30 11:46:20 +0000

    Staging orders done

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -3,13 +3,57 @@
 }}
 
 with orders as (
-    SELECT *,
+    SELECT
+    -- from raw orders 
+    orderid,
+    orderdate,
+    shipdate,
+    shipmode,
+    customerid,
+    productid,
     ordersellingprice - ordercostprice as orderprofit
-    FROM RAW.GLOBALMART.ORDERS
+    FROM {{ ref('raw_orders')}}
+),
+
+customer as (
+    SELECT 
+    customername,
+    segment,
+    country,
+    customerid
+    FROM {{ref('raw_customer')}}
+),
+
+product as (
+    SELECT
+    category,
+    productname, 
+    subcategory,
+    productid
+    from {{ ref('raw_product')}}
+),
+
+customer_orders as (
+    SELECT 
+    o.orderid,
+    o.orderdate,
+    o.shipdate,
+    o.shipmode,
+    o.orderprofit,
+    c.customername,
+    c.segment,
+    c.country,
+    p.category,
+    p.productname, 
+    p.subcategory
+    FROM orders as o
+    LEFT JOIN customer AS c ON orders.customerid = customer.customerid
+    LEFT JOIN product AS p ON orders.productid = product.productid
 ),
 
 final as (
-    SELECT * FROM orders
+    SELECT *
+    FROM customer_orders
 )
 
 SELECT * FROM final
