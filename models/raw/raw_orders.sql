2363ab15d1adba87a0cf963a9d7c8fce02e75160
appdev87 <appdev@hectorcode.com>
2022-05-30 09:41:09 +0000

    Abiding to the recommended. dbt format

--- models/raw/raw_orders.sql
+++ models/raw/raw_orders.sql
@@ -1,1 +1,13 @@
-SELECT * FROM RAW.GLOBALMART.ORDERS
+{{
+    config(amterialized='table')
+}}
+
+with orders as (
+    SELECT * FROM RAW.GLOBALMART.ORDERS
+),
+
+final as (
+    SELECT * FROM orders
+)
+
+SELECT * FROM final
